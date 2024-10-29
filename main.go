22927afdc55b5e8661ae5485b3ec57dc1d3434ad
Nitin Chauhan <114130969+ashborn3@users.noreply.github.com>
2024-10-29 14:32:05 +0530

    use goroutines for concurrent processing

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
-var dirMap = make(map[string][]string)
+var (
+	dirMap = make(map[string][]string)
+	mu     sync.Mutex
+	wg     sync.WaitGroup
+)
 
 func main() {
 	homeDir := "/"
@@ -19,12 +24,17 @@ func main() {
 	for _, entry := range rootDir {
 		if entry.IsDir() && entry.Name() != "mnt" {
 			fmt.Printf("Directory: %s\n", entry.Name())
-			walk(homeDir + entry.Name())
+			wg.Add(1)
+			go walk(homeDir + entry.Name())
 		}
 	}
+
+	wg.Wait()
 }
 
 func walk(path string) {
+	defer wg.Done()
+
 	dirContent, err := os.ReadDir(path)
 	if err != nil {
 		fmt.Printf("Error reading directory: %s\n", err)
@@ -34,13 +44,16 @@ func walk(path string) {
 	for _, entry := range dirContent {
 		if entry.IsDir() {
 			fmt.Printf("Directory: %s\n", entry.Name())
-			walk(path + "/" + entry.Name())
+			wg.Add(1)
+			go walk(path + "/" + entry.Name())
 		} else {
 			fmt.Printf("File: %s\n", entry.Name())
+			mu.Lock()
 			dirMap[entry.Name()] = append(
 				dirMap[entry.Name()],
 				filepath.Join(path, entry.Name()),
 			)
+			mu.Unlock()
 		}
 	}
 }
