62bb58179bbaf689bac21a9248f0d878296b0ef9
Nitin Chauhan <114130969+ashborn3@users.noreply.github.com>
2024-10-31 13:10:36 +0530

    Add configuration loading functionality with JSON support for Client

---
+++ client/config.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	HomePath      string `json:"homePath"`
+	CachePath     string `json:"cachePath"`
+	EncryptionKey string `json:"encryptionKey"`
+	Ip            string `json:"ip"`
+	Port          int    `json:"port"`
+	SyncTime      int    `json:"syncTime"`
+}
+
+func loadConfig(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var config Config
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
